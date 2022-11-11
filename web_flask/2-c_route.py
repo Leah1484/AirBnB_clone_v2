60d30757b55a95f037dd9c407d1a8ef06a108c10
Leah1484 <86114205+Leah1484@users.noreply.github.com>
2022-11-11 17:10:36 +0300

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
+"""Starts a Flask web application.
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
-    /hbnb: Displays 'HBNB'
-    /c/<text>: Displays 'C' followed by the value of the text variable
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@pp.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return 'Hello HBNB!'
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'"""
-    return 'HBNB'
+    """Displays 'HBNB'."""
+    return "HBNB"
 
 
-@pp.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Displays 'C' followed by the value of the text variable"""
-    return 'C {}'.format(text.replace('_', ' '))
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0")
